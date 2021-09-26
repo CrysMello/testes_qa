dfba12e7631140ef574df91251142ec9a05de9c6
CrysMello <crystianemello@hotmail.com>
2021-09-25 23:45:24 -0300

    alteração função fosforo

--- fosforo/main.py
+++ fosforo/main.py
@@ -7,7 +7,21 @@
 # usuario: int(input("Quantas caixas de fosfóros? "))
 
 
+def calcular_fosforos(num1, num2):
+    return num1 * num2
 
 
+def teste_fosforos():
+    # configura
+
+    num1 = 10
+    num2 = 40
+
+
+resultado_esperado = 400
+
+# parte 2 executa
+
+resultado_atual = calcular_fosforos(num1, num2)
 
 assert resultado_esperado == resultado_atual
