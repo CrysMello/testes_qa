a621de7fd4bd34960ca55efd1fbbc04110381a11
CrysMello <crystianemello@hotmail.com>
2021-09-30 13:24:49 -0300

    refatoração da função divisão

--- ConstrutoPYa/main.py
+++ ConstrutoPYa/main.py
@@ -1,7 +1,9 @@
 # import bibliotecas
 
 import pytest
-#2-class
+
+
+# 2-class
 
 def print_hi(name):
     print(f'Oi, {name}')
@@ -23,27 +25,35 @@ def dividir(num1, num2):
     if num2 != 0:
         return num1 / num2
     else:
-        resultado = "Não dividiras por zero"
-
-def dividir_try_except(num1,num2):
-    try:
-        return num1 /num2
-    except:
         return "Não dividiras por zero"
 
- # parte 2 executa
-    resultado_atual = somar(num1, num2,resultado)
+# refatoração  do teste de divisão
+def dividir_try_except(num1, num2):
+    try:
+        return num1 / num2
+    except TypeError:
+        return TypeError
+
+
+
+    # parte 2 executa
+    resultado_atual = somar(num1, num2, resultado)
 
     # parte 3 check / valida
     assert resultado_atual == resultado_esperado
+
+
 # teste unitarios/ teste unidades
 
 # teste de função somar
-@pytest.mark.parametrize('num1,num2,resultado',[
-    #valores
-    (5, 4, 9), # teste 1
-    (3, 2, 5), # teste 2
-    (10,6, 15), # teste 3
+
+# teste positivo -- mostra o resultado correto
+#                -- avança para aproxima etapa
+@pytest.mark.parametrize('num1,num2,resultado', [
+    # valores
+    (5, 4, 9),  # teste 1
+    (3, 2, 5),  # teste 2
+    (10, 6, 15),  # teste 3
 ])
 def test_somar(num1, num2, resultado):
     try:
@@ -51,8 +61,9 @@ def test_somar(num1, num2, resultado):
     except AssertionError:
         print(f'Entrou no Except: {AssertionError}')
 
+
 def teste_somar_resultado_negativo():
-    assert somar(-1000,-2000) == -3000
+    assert somar(-1000, -2000) == -3000
 
 
 def test_subtrair():
@@ -66,13 +77,20 @@ def test_mutiplicar():
 def test_dividir():
     assert dividir(10, 2) == 5
 
+# teste negativo -- mostra a mensagem de erro
 def teste_dividir_por_zero():
-    assert dividir(8,0) == "Não dividiras por zero"
+    assert dividir(8, 0) == "Não dividiras por zero"
+@pytest.mark.parametrize("num1, num2, resultado",[
+    (8,2,4),
+    (20,4,5),
+    (10,0,"Não dividiras por zero")
+])
+def test_dividir_try_except(num1,num2,resultado):
+    assert dividir_try_except(num1,num2) == resultado
 
-def test_dividir_try_except_por_zero():
-    assert dividir_try_except(8,0)
 
-#TDD : Desenvolvimento Direcionado pelo Testes
+
+# TDD : Desenvolvimento Direcionado pelo Testes
 # - Criar o esqueleto de classes, funções e métodos logo no início da Sprint
 # - Criar pelo 1 teste (feliz) para todas as funções e métodos
 # - Executar todos os testes unitários diariamente para medir o progresso
