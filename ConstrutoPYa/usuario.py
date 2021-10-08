8fabbfc7d6951ca51311a79b76c43d30ae76a25a
CrysMello <crystianemello@hotmail.com>
2021-10-08 13:52:52 -0300

    teste de integração: função testa dados usuario

---
+++ ConstrutoPYa/usuario.py
@@ -0,0 +1,49 @@
+# 1- imports
+import pytest
+import csv
+import json
+import requests
+from requests import HTTPError
+
+teste_dados_usuarios = [
+    (1, "Juca", "Pirama", "juca@iterasys.com.br"),          # usuario 1
+    (2, "Agatha", "Christie", "agatha@iterasys.com.br")     # usuario 2
+]
+# teste de integração
+
+teste_dados_usuarios_atuais = [
+    (1, "George", "Bluth", "george.bluth@reqres.in"),  # usuario 1
+    (2, "Janet", "Weaver", "janet.weaver@reqres.in")   # usuario 2
+]
+@pytest.mark.parametrize("id,nome,sobrenome,email",teste_dados_usuarios_atuais)
+def testar_dados_usuarios(id,nome,sobrenome,email): #função que testa o algo
+    try:
+      response = requests.get(f'https://regres;in/api/users:{id}')
+      jsonResponse = response.json()
+      id_obtido = jsonResponse['data']['id']
+      nome_obtido = jsonResponse['data']['first_name']
+      sobrenome_obtido = jsonResponse['data']['last_name']
+      email_obtido = jsonResponse['data']['email']
+
+      print(f'id: {id_obtido} \n nome: {nome_obtido} \n sobrenome: {sobrenome_obtido} \n email: {email_obtido}')
+      print(f'id: {id_obtido} - nome: {nome_obtido} - sobrenome: {sobrenome_obtido} - email: {email_obtido}')
+      print('id:{} \n nome:{} \n sobrenome:{} \n email:{}'.format(id_obtido, nome_obtido, sobrenome_obtido, email_obtido))
+      print(json.dumps(jsonResponse, indent=2, sort_keys=True))
+
+      assert id_obtido == id
+      assert nome_obtido == nome
+      assert sobrenome_obtido == sobrenome
+      assert email_obtido == email
+
+    except HTTPError as http_fail:
+        print(f'Um erro de HTTP aconteceu: {http_fail}')
+    except Exception as fail: #qualquer exceção serpa tratada a seguir
+        print(f'falha inesperada: {fail}')
+
+
+
+# função que faz algo --> Fora do Computador
+#API para fazer o teste:
+# https://regres;in/api/user:{id}
+
+
